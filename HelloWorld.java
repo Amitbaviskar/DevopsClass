ae7bc4bbc121d3ff2f3c2853e5d6064d3e52833c
Amitbaviskar <106909464+Amitbaviskar@users.noreply.github.com>
2026-04-23 11:01:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I am in devops class with Vikas Ratnawat Sir");
+        System.out.println("Hello, World! Current build executed at: " + java.time.LocalDateTime.now());
     }
 }
